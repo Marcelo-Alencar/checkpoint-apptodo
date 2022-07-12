6e965cd59917513125168c7743b09679f34a6d9c
ArianeSanchez <ariane.sanchez1@gmail.com>
2022-07-11 22:33:13 -0300

    Atualização de cadastro signup

--- scripts/signup.js
+++ scripts/signup.js
@@ -12,6 +12,18 @@ var formValidation =  {
     userPasswordConfirm: false
 }
 
+var formData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmpassword: ""   
+}
+
+var requestHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
 
 buttonCreateUserElement.addEventListener('click', event => {
 
@@ -24,12 +36,57 @@ buttonCreateUserElement.addEventListener('click', event => {
 
         window.location = './index.html'
     } else {
-        alert('o formulario nao esta preenchido corretamente.')
+        alert('O formulário não está preenchido corretamente.')
     }
 
 
 
 })
+
+var requestPostConfiguration = {
+    method: 'POST',
+    headers: requestHeaders
+}
+
+function createUser() {
+  
+    requestPostConfiguration.body = JSON.stringify(formData)
+
+// O Fetch é responsável por fazer uma requisição para um back-end
+// O parametro do fetch serve justamente para especificarmos aonde ele irá fazer a requisição
+
+fetch('https://ctd-fe2-todo-v2.herokuapp.com/v1/users', requestPostConfiguration).then(
+
+    response => {
+
+        response.json().then(
+
+            success => {
+
+                if(response.ok) {
+
+                   location.href = './index.html'
+
+                } else {
+
+                    if(success === 'El usuario ya se encuentra registrado') {
+
+                        alert('O e-mail digitado já está cadastrado')
+
+                    }
+
+                }
+
+            }
+
+        )
+
+
+    }
+
+)
+}
+
 
 for(let control of formControlsElements) {
 
@@ -69,3 +126,7 @@ userPasswordConfirmElement.addEventListener('keyup', event => {
 
   })
 
+  
+  
+ 
+  
